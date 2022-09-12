52bbcac903d224da75cfb7faad387542a63900db
unknown <Farmcias App@NA903448DEVBNU.gruposc.local>
2022-09-12 19:40:03 -0300

    first commit

---
+++ src/test/java/testesenai/testesaitesenai.java
@@ -0,0 +1,33 @@
+package testesenai;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testesaitesenai {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void abrirNavegador() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\cromerdrave\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	
+	@Test
+	public void testeNavegador() {
+		
+		driver.get("https://informatica.sp.senai.br/");
+		driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("Excel", Keys.RETURN);
+		
+		
+		
+	}
+
+}
